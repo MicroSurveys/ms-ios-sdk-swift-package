3efbe4a3e1231769412db6f21624e688ea1ff7cf
Jorge Pereira <jorgefspereira@icloud.com>
2024-12-09 16:42:45 +0000

    first commit

--- Sources/microsurveys/microsurveys.swift
+++ Sources/microsurveys/microsurveys.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct microsurveys {
+    public static func doSomething() {
+        print("Hello, World!")
+    }
+}
