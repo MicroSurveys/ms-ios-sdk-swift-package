eb1223acaa9726ed007f54e323df5313e80389d6
Jorge Pereira <jorgefspereira@icloud.com>
2024-12-13 15:49:23 +0000

    adding built framework

--- Package.swift
+++ Package.swift
@@ -5,17 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "microsurveys",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "microsurveys",
-            targets: ["microsurveys"]),
+        .library( name: "microsurveys", targets: ["microsurveys"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "microsurveys"),
-
+        .binaryTarget(name: "microsurveys", path: "./Sources/microsurveys.xcframework"),
     ]
 )
---
+++ Sources/microsurveys.xcframework/ios-arm64/microsurveys.framework/Headers/microsurveys.h
@@ -0,0 +1,18 @@
+//
+//  microsurveys.h
+//  microsurveys
+//
+//  Created by Jorge Pereira on 28/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for microsurveys.
+FOUNDATION_EXPORT double microsurveysVersionNumber;
+
+//! Project version string for microsurveys.
+FOUNDATION_EXPORT const unsigned char microsurveysVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <microsurveys/PublicHeader.h>
+
+
